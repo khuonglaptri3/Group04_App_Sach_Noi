ff0fbdd4d06bc20a3d8573665afe6affa1720ff1
Dinh Khuong <lanphuongtran1999@gmail.com>
2026-05-15 10:43:42 +0700

    feature 03 - Homescreen UI / Dependance

--- app/src/main/java/com/example/a23110035_23110060/MainActivity.java
+++ app/src/main/java/com/example/a23110035_23110060/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a23110035_23110060;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -24,11 +25,23 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        // Nút sang Sign Up
         TextView tvSignUp = findViewById(R.id.tvSignUp);
         tvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Nút đăng nhập → mở HomeActivity
+        Button btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
             }
         });
